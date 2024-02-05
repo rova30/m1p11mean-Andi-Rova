964a86c8b689c649090eadc1b4f273e47da50009
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2024-02-05 19:10:32 +0300

    great connection mongo node

--- back-end/application/index.js
+++ back-end/application/index.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const port = 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://webavanceem1:final@clusterm1.kqgspnb.mongodb.net/?retryWrites=true&w=majority";
 
-// Connexion à la base de données
-mongoose.connect('mongodb+srv://webavanceem1:final@clusterm1.kqgspnb.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-// Gestion des erreurs de connexion
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB :'));
-db.once('open', () => {
-  console.log('Connexion à MongoDB Atlas établie avec succès');
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-// Définir des routes et des gestionnaires ici
-app.listen(port, () => {
-  console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
-});
-
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
