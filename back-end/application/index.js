793073c4eebe798384fb31e78c894c9577fd9ea6
rova30 <rovarobel30@gmail.com>
2024-02-05 10:22:19 +0300

    update init back-end expressJs

--- back-end/application/index.js
+++ back-end/application/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+// Définir des routes et des gestionnaires ici
 
 app.listen(port, () => {
   console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Salama boss !');
 });
