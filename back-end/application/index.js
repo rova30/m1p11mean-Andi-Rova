f6fa993e93571984ee997e951c060ac09841d58c
P14BAndi2003 <andi.nomenjanahary2003@gmail.com>
2024-02-06 14:14:49 +0300

    update for structure with router

--- back-end/application/index.js
+++ back-end/application/index.js
@@ -1,38 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-
 const app = express();
 const port = 3000;
-const connectionString = 'mongodb+srv://webavanceem1:final@clusterm1.kqgspnb.mongodb.net/?retryWrites=true&w=majority'; // Remplacez par votre chaîne de connexion MongoDB
 
-app.use(bodyParser.json());
+const customerRoutes = require('./routes/customerRoutes');
 
-app.post('/addCustomer', async (req, res) => {
-  try {
-    const client = await MongoClient.connect(connectionString, { useUnifiedTopology: true });
-    const db = client.db('finalexam');
-    if (!req.body) {
-      return res.status(400).json({ error: 'Request body is missing or invalid' });
-    }
-    const { firstName, lastName, contact, email, password, virtualWallet } = req.body;
-    const newCustomer = {
-      firstName,
-      lastName,
-      contact,
-      email,
-      password,
-      virtualWallet
-    };
-    const result = await db.collection('Customers').insertOne(newCustomer);
-    res.json({ message: 'Customer added successfully' });
-    client.close();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error adding customer' });
-  }
-});
+// this is the prefix eny @url avant de faire le post
+app.use('/api/customers', customerRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
---
+++ back-end/application/routes/customerRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const connectionString = 'mongodb+srv://webavanceem1:final@clusterm1.kqgspnb.mongodb.net/?retryWrites=true&w=majority';
+
+router.use(bodyParser.json());
+// tout ce qui est rattaché au customer
+router.post('/addCustomer', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(connectionString, { useUnifiedTopology: true });
+    const db = client.db('finalexam');
+    if (!req.body) {
+      return res.status(400).json({ error: 'Request body is missing or invalid' });
+    }
+    const { firstName, lastName, contact, email, password, virtualWallet } = req.body;
+    const newCustomer = {
+      firstName,
+      lastName,
+      contact,
+      email,
+      password,
+      virtualWallet
+    };
+    const result = await db.collection('Customers').insertOne(newCustomer);
+    res.json({ message: 'Customer added successfully' });
+    client.close();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error adding customer' });
+  }
+});
+
+module.exports = router;
