d1c97a6f42410872e72705102765755d0ab4de58
P14BAndi2003 <andi.nomenjanahary2003@gmail.com>
2024-02-09 20:08:49 +0300

    Update app.module.ts

--- front-office/now-ui-kit-angular-master/src/app/app.module.ts
+++ front-office/now-ui-kit-angular-master/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { AuthService } from './api/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 
+
 @NgModule({
     declarations: [
         AppComponent,
