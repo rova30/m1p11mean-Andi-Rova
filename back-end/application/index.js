c2af0dea590063f65ad964ce750390f0f39f89bc
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2024-02-11 19:34:09 +0300

    addnewEmployee+upload

--- back-end/application/index.js
+++ back-end/application/index.js
@@ -1,8 +1,10 @@
+// Importer les modules nécessaires
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
+// Importer les routes
 const customerRoutes = require('./routes/customerRoutes');
 const expensesRoutes = require('./routes/expenseRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
@@ -10,7 +12,10 @@ const employeeRoutes = require('./routes/employeeRoutes');
 const specialOfferRoutes = require('./routes/specialOfferRoutes');
 const preferenceRoutes = require('./routes/preferenceRoutes');
 
+// Utiliser cors pour autoriser les requêtes de différents domaines
 app.use(cors());
+
+// Utiliser les routes dans l'application
 app.use('/customers', customerRoutes);
 app.use('/expenses', expensesRoutes);
 app.use('/services', serviceRoutes);
@@ -18,6 +23,7 @@ app.use('/employees', employeeRoutes);
 app.use('/specialOffers', specialOfferRoutes);
 app.use('/preferences', preferenceRoutes);
 
+// Démarrer le serveur
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
--- back-office/now-ui-dashboard-angular-master/src/app/manager/employee/employee.component.ts
+++ back-office/now-ui-dashboard-angular-master/src/app/manager/employee/employee.component.ts
@@ -6,12 +6,20 @@ import { EmployeeService } from '../../api/employee.service';
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-
 export class EmployeeComponent implements OnInit {
   employees: any[] = [];
   error: string = '';
   currentPage: number = 1; 
   totalPages: number = 0; 
+  employeeData: any = {
+    firstName: '',
+    lastName: '',
+    address: '',
+    contact: '',
+    email: '',
+    password: '',
+    photo: null
+  };
 
   constructor(private employeeService: EmployeeService) { }
 
@@ -35,6 +43,39 @@ export class EmployeeComponent implements OnInit {
     );
   }
 
+  addEmployee() {
+    
+    this.convertImageToBase64(this.employeeData.photo).then((base64Image: string) => {
+      this.employeeData.photo = base64Image;  
+      this.employeeService.addEmployee(this.employeeData).subscribe(
+        (response) => {
+          console.log('Employee added successfully:', response);
+          this.resetForm();
+          this.getEmployees(this.currentPage, 10); 
+        },
+        (error) => {
+          console.error('Error adding employee:', error);
+          
+        }
+      );
+    }).catch((error) => {
+      console.error('Error converting image to base64:', error);
+      
+    });
+  }
+  
+
+  resetForm() {
+    this.employeeData = {
+      firstName: '',
+      lastName: '',
+      address: '',
+      contact: '',
+      email: '',
+      password: '',
+      photo: null
+    };
+  }
 
   prevPage() {
     if (this.currentPage > 1) {
@@ -42,10 +83,27 @@ export class EmployeeComponent implements OnInit {
     }
   }
 
-
   nextPage() {
     if (this.currentPage < this.totalPages) {
       this.getEmployees(this.currentPage + 1, 10); 
     }
+  }
+
+  onFileSelected(event: any) {
+    this.employeeData.photo = event.target.files[0];  
+  }
+
+ 
+  convertImageToBase64(imageFile: File): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(imageFile);
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = (error) => {
+        reject(error);
+      };
+    });
   }
 }
