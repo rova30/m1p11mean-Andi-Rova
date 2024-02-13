f7c2503fda1f153d41549508044a5198feb502c2
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2024-02-13 21:34:30 +0300

    first step add new special offer

--- back-office/now-ui-dashboard-angular-master/src/app/manager/specialoffer/specialoffer.component.ts
+++ back-office/now-ui-dashboard-angular-master/src/app/manager/specialoffer/specialoffer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SpecialOfferService } from '../../api/specialoffer.service';
+import { ServiceService } from '../../api/service.service'; // Importer le service ServiceService
 
 @Component({
   selector: 'app-specialoffer',
@@ -12,7 +13,6 @@ export class SpecialOfferComponent implements OnInit {
     currentPage: number = 1;
     pageSize: number = 10;
     totalPages: number = 0;
-  
     loading: boolean = false;
     serviceData: any = {
       name: '',
@@ -20,15 +20,43 @@ export class SpecialOfferComponent implements OnInit {
       reduction: '',
       price: '',
       dateStart: '',
-      dateEnd: ''
+      dateEnd: '',
+      services: [] // Ajouter un tableau pour stocker les services sélectionnés
     };
-  
-    constructor(private specialOfferService: SpecialOfferService) { }
+
+    selectedService: any;
+    services: any[] = []; // Array to store services available
+
+    constructor(private specialOfferService: SpecialOfferService, private serviceService: ServiceService) { } // Injecter le service ServiceService
   
     ngOnInit() {
       this.getSpecialOffersCount();
+      this.loadServices(); 
     }
-  
+
+    loadServices() {
+      this.serviceService.getServices(1, 100).subscribe(
+        (data: any[]) => {
+          this.services = data;
+        },
+        (error: any) => {
+          console.error('Error fetching services:', error);
+        }
+      );
+    }
+
+
+    addService() {
+      if (this.selectedService) {
+        this.serviceData.services.push(this.selectedService);
+        this.selectedService = null; 
+      }
+    }
+
+    removeService(index: number) {
+      this.serviceData.services.splice(index, 1);
+    }
+
     getSpecialOffersCount() {
       this.specialOfferService.totalSpecialOffersCount().subscribe(
         (count: number) => {
@@ -60,39 +88,37 @@ export class SpecialOfferComponent implements OnInit {
     }
   
     addSpecialOffer() {
-        this.specialOfferService.addSpecialOffer(this.serviceData).subscribe(
-          (response) => {
-            console.log('service added successfully:', response);
-            this.resetForm();
-            this.getSpecialOffers(this.currentPage, this.pageSize);
-          },
-          (error) => {
-            console.error('Error adding service:', error);
-           
-          }
-        );
-      }
+      this.specialOfferService.addSpecialOffer(this.serviceData).subscribe(
+        (response) => {
+          console.log('service added successfully:', response);
+          this.resetForm();
+          this.getSpecialOffers(this.currentPage, this.pageSize);
+        },
+        (error) => {
+          console.error('Error adding service:', error);
+        }
+      );
+    }
 
-      isExpired(dateEnd: string): boolean {
-        const now = new Date();
-        const endDate = new Date(dateEnd);
-        const endYear = endDate.getFullYear();
-        const endMonth = endDate.getMonth();
-        const endDay = endDate.getDate();
-        const end = new Date(endYear, endMonth, endDay);
-        return end < now;
-      }
+    isExpired(dateEnd: string): boolean {
+      const now = new Date();
+      const endDate = new Date(dateEnd);
+      const endYear = endDate.getFullYear();
+      const endMonth = endDate.getMonth();
+      const endDay = endDate.getDate();
+      const end = new Date(endYear, endMonth, endDay);
+      return end < now;
+    }
       
-  
     resetForm() {
       this.serviceData = {
-        firstName: '',
-        lastName: '',
-        address: '',
-        contact: '',
-        email: '',
-        password: '',
-        photo: null
+        name: '',
+        description: '',
+        reduction: '',
+        price: '',
+        dateStart: '',
+        dateEnd: '',
+        services: [] // Reset also the services array
       };
     }
   
@@ -107,5 +133,4 @@ export class SpecialOfferComponent implements OnInit {
         this.getSpecialOffers(this.currentPage + 1, this.pageSize);
       }
     }
-  
 }
