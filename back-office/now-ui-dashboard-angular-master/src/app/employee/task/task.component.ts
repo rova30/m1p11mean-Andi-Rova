67afc525dc5c941eeae12105e7d7d8dd1c337f9d
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2024-02-25 12:00:02 +0300

    no need comments

--- back-office/now-ui-dashboard-angular-master/src/app/employee/task/task.component.ts
+++ back-office/now-ui-dashboard-angular-master/src/app/employee/task/task.component.ts
@@ -12,22 +12,20 @@ export class TaskComponent implements OnInit {
   employee: any;
   error: string;
   loading: boolean = false;
-  dataAssignment: any; // Variable pour stocker les données
+  dataAssignment: any; 
   totalCommissionSum: number = 0;
-  selectedDate: string; // Variable pour stocker la date sélectionnée
+  selectedDate: string; 
 
   constructor(private employeeService: employeeService.EmployeeService, private assignmentService: AssignmentAppointmentService) { }
 
   ngOnInit() {
     const token = sessionStorage.getItem('token_employee');
     this.getEmployee(token);
-    // Initialisez la date sélectionnée avec la date d'aujourd'hui par défaut
     this.selectedDate = new Date().toISOString().split('T')[0];
   }
 
   formatTime(dateTime: string): string {
-    // Utiliser moment.js pour formater la date
-    return moment(dateTime).format('HH:mm'); // 'HH:mm' pour afficher l'heure au format 24h
+    return moment(dateTime).format('HH:mm'); 
   }
 
   getEmployee(token: string) {
@@ -37,7 +35,7 @@ export class TaskComponent implements OnInit {
         this.employee = data.employee;
         this.loading = false;
         if (this.employee) {
-          const statut = 2; // Changez le statut selon vos besoins
+          const statut = 2; 
           this.getAppointmentsByEmployeeId(this.employee._id, this.selectedDate, statut);
         } else {
           console.error('Employee is not defined yet.');
@@ -56,7 +54,7 @@ export class TaskComponent implements OnInit {
     this.loading = true;
     this.assignmentService.getassignmentByEmployee(employeeId, date, statut).subscribe(
       (data: any) => {
-        this.dataAssignment = data; // Stockez les données récupérées
+        this.dataAssignment = data; 
         this.totalCommissionSum = this.dataAssignment.reduce((acc, curr) => acc + curr.totalCommission, 0);
         this.loading = false;
       },
@@ -69,8 +67,7 @@ export class TaskComponent implements OnInit {
   }
 
   onDateChange() {
-    // Appeler la fonction pour récupérer les rendez-vous avec la nouvelle date sélectionnée
-    const statut = 2; // Changez le statut selon vos besoins
+    const statut = 2; 
     this.getAppointmentsByEmployeeId(this.employee._id, this.selectedDate, statut);
   }
 }
