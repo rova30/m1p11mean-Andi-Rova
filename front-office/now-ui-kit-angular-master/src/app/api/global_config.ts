708d389d4feb890b9a4e37157900b830239f9892
rova30 <rovarobel30@gmail.com>
2024-02-29 23:38:21 +0300

    config prod

--- front-office/now-ui-kit-angular-master/src/app/api/global_config.ts
+++ front-office/now-ui-kit-angular-master/src/app/api/global_config.ts
@@ -1,6 +1,6 @@
-const apiUrl = " http://localhost:3000";
-//
-//const apiUrl = "http://192.168.88.91:8000/api/";
+//const apiUrl = "http://localhost:3000";
+const apiUrl = "https://m1p11mean-andi-rova-backend.onrender.com"
+
 const baseUrl = (str: string) => apiUrl + str;
 
 export {baseUrl}
