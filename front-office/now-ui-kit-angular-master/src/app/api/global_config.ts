0d48b2f5a9691b560d3d8974336abc3d37010eed
rova30 <rovarobel30@gmail.com>
2024-03-01 17:45:40 +0300

    fix bug affichage service

--- front-office/now-ui-kit-angular-master/src/app/api/global_config.ts
+++ front-office/now-ui-kit-angular-master/src/app/api/global_config.ts
@@ -1,5 +1,5 @@
-//const apiUrl = "http://localhost:3000";
-const apiUrl = "https://m1p11mean-andi-rova-backend.onrender.com"
+const apiUrl = "http://localhost:3000";
+//const apiUrl = "https://m1p11mean-andi-rova-backend.onrender.com"
 
 const baseUrl = (str: string) => apiUrl + str;
 
--- front-office/now-ui-kit-angular-master/src/app/views/service/service.component.ts
+++ front-office/now-ui-kit-angular-master/src/app/views/service/service.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ServiceService } from '../../api/service.service';
 import Swal from 'sweetalert2';
 import { AppointmentService } from 'app/api/appointment.service';
+import { CommonModule  } from '@angular/common';
 
 @Component({
   selector: 'app-service',
@@ -20,20 +21,69 @@ export class ServiceComponent implements OnInit {
     selectedServices: any[] = [];
     totalServicesDuration: number = 0;
     avalaibleDate: any[] = [];
+    formattedData: any[] = [];
 
 
-
-    constructor(private serviceService: ServiceService,private appointmentService: AppointmentService) { }   
+    constructor(private serviceService: ServiceService,private appointmentService: AppointmentService) {
+     }   
     ngOnInit() {
       this.getServices();
       this.getServicesCount();
+
     }
 
+    formatData(data: any[]) {
+      this.formattedData = [];
+  
+      data.forEach((jour) => {
+        const jourIndex = Object.keys(jour)[0];
+        const heures = jour[jourIndex];
+  
+        const formattedHeures = heures.map((heure: string) => {
+          return new Date(heure).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
+        });
+  
+        this.formattedData.push({
+          jour: new Date(heures[0]).toLocaleDateString('fr-FR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          }),
+          heures: formattedHeures,
+        });
+
+        console.log(this.formattedData)
+      });
+    }
+
+    
+    afficherDateHeure(jour: string, heure: string) {
+      // Concaténer la date et l'heure
+      const dateHeureSelectionnee = `${jour} ${heure}`;
+    
+      // Convertir la chaîne en objet Date
+      const dateObj = new Date(dateHeureSelectionnee);
+    
+      // Extraire les composants de la date
+      const year = dateObj.getFullYear();
+      const month = String(dateObj.getMonth() + 1).padStart(2, '0'); // Les mois sont de 0 à 11, donc on ajoute 1 et on remplit éventuellement avec un zéro à gauche
+      const day = String(dateObj.getDate()).padStart(2, '0');
+      const hours = String(dateObj.getHours()).padStart(2, '0');
+      const minutes = String(dateObj.getMinutes()).padStart(2, '0');
+    
+      // Formater la date au format 'yyyy-MM-dd HH:mm'
+      const dateFormatee = `${year}-${month}-${day} ${hours}:${minutes}`;
+    
+      console.log(dateFormatee);
+    }
+    
+    
     getAvalaibleDate(deadline:number) {
       if(this.totalServicesDuration != 0){
         this.appointmentService.getAvailableDate(deadline).subscribe(
           (data: any[]) => {
             this.avalaibleDate = data;
+            this.formatData(this.avalaibleDate);
           },
           (error: any) => {
             console.error('Error fetching avalaibleDate:', error);
@@ -94,13 +144,13 @@ export class ServiceComponent implements OnInit {
       this.totalServicesDuration += selectedService.deadline;
       this.getAvalaibleDate(this.totalServicesDuration);
 
+
   }
 
   removeService(index: number,selectedService: any) {
     this.selectedServices.splice(index, 1);
     this.totalServicesDuration -= selectedService.deadline;
     this.getAvalaibleDate(this.totalServicesDuration);
-
   }
 
 
